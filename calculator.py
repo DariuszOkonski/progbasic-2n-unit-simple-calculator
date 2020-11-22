19dd7f4c720fcba7330667c2cd18331f84ad8f1a
Dariusz Okonski <darek200180@gmail.com>
2020-11-22 21:08:46 +0000

    add spaces for readibilyty

--- calculator.py
+++ calculator.py
@@ -2,22 +2,25 @@ def is_number(input_number):
     try:
         number = float(input_number)
         if number >= 0:
-            return number
+            return number    
     except ValueError:
-        pass
+        pass    
     except TypeError:
         pass
+    
     return None
 
 
 def is_valid_operator(operator):
     operators = ('+', '-', '*', '/')
+    
     return operator in operators
 
 
 def ask_for_a_number(force_valid_input):    
     while True:
         number = is_number(input("Please provide a number: "))
+    
         if(number == None and force_valid_input):
             print("This didn't look like a number,try again")
             continue
@@ -29,6 +32,7 @@ def ask_for_an_operator(force_valid_input):
     while True:
         operator = (input("Please provide an operator(one of: +, -, *, /): "))
         valid_operator = is_valid_operator(operator)
+        
         if(not valid_operator and force_valid_input):
             print("Unknown operator")
             continue
@@ -39,7 +43,7 @@ def ask_for_an_operator(force_valid_input):
 def calc(operator, a, b):
     try:
         a = float(a)
-        b = float(b)
+        b = float(b)    
     except ValueError:
         return None
 
@@ -55,7 +59,7 @@ def calc(operator, a, b):
     if(operator == '/'):
         if b == 0:
             print("Division by zero")
-            return None
+            return None    
         result = a / b
     
     return round(result, 2)
